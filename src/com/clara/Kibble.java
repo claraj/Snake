c7a34a170229a6d7512cc9ce204708ba256698fa
Clara <minneapolis-edu@users.noreply.github.com>
2015-11-25 17:16:50 -0600

    Update Kibble.java

--- src/com/clara/Kibble.java
+++ src/com/clara/Kibble.java
@@ -2,11 +2,15 @@ package com.clara;
 
 import java.util.Random;
 
+/* In this game, Snakes eat Kibble. Feel free to rename to SnakeFood or Prize or Treats or Cake or whatever. */
+
+
 public class Kibble {
 
 	/** Identifies a random square to display a kibble
 	 * Any square is ok, so long as it doesn't have any snake segments in it. 
-	 * There is only one Kibble and when the snake eats it, then it moves. 
+	 * There is only one Kibble that knows where it is on the screen. When the snake eats the kibble, it doesn't disapear and
+	 * get recreated, instead it moves, and then will be drawn in the new location. 
 	 */
 	
 	private int kibbleX; //This is the square number (not pixel)
